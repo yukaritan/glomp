89dc942c10ea0278becf43a5d18c326f5342a601
yukaritan <yukaritan@users.noreply.github.com>
2015-02-14 21:17:42 +0100

    birth!

---
+++ index.py
@@ -0,0 +1,18 @@
+import upload
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/upload")
+def upload():
+    return "Hello upload!"
+
+
+if __name__ == "__main__":
+    app.run()
